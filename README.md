d31943b534e3732ff44ac26ce07b98948288cba4
Nikorasu <nikorasu85@gmail.com>
2021-03-28 17:48:50 -0500

    Tweaked comments, added more to Readme.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 This is my attempt at recreating a [Boids simulation](https://en.wikipedia.org/wiki/Boids "Wikipedia"), written in Python3 with Pygame2.
 
 **To use**, save the `pynboids.py` file (and `nboids.png` if you want it's icon, not required) somewhere, and run via python.
-I've included several tweakable settings near the top of the code. There you can adjust window size, fullscreen, fps, as well as whether boids avoid edges or wrap to the other side.
+I've included several tweakable settings near the top of the code. You can adjust window size, fullscreen, fps, and how many boids to spawn, as well as whether boids avoid screen edges or wrap to the other side.
 
 ![Preview](preview.gif "Preview")
-
--- pynboids.py
+++ pynboids.py
@@ -3,9 +3,9 @@ from math import sin, cos, atan2, radians, degrees
 from random import randint
 
 #  PyNBoids by Nik - a Boids simulation
-FULLSCREEN = False  # Fullscreen or Window
-BOIDZ = 100         # how many boids to spawn, may slow after 100-200ish
-WRAP = False        # wrap boids to other side of screen, otherwise avoid edge.
+FULLSCREEN = False  # True for Fullscreen or False for Window
+BOIDZ = 100         # How many boids to spawn, may slow after 100-200ish.
+WRAP = False        # Wrap boids to other side of screen, otherwise avoid edge.
 WIDTH = 1200        # 1200
 HEIGHT = 800        # 800
 FPS = 48            # 30-90
@@ -94,7 +94,8 @@ def main():
     pg.display.set_caption("PyNBoids")
     try: pg.display.set_icon(pg.image.load("nboids.png"))
     except: print("FYI: nboids.png icon not found, skipping..")
-    if FULLSCREEN:  # screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+    if FULLSCREEN:
+        #screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
         currentRez = (pg.display.Info().current_w, pg.display.Info().current_h)
         screen = pg.display.set_mode(currentRez, pg.FULLSCREEN | pg.SCALED)
         pg.display.toggle_fullscreen()  # linux workaround
